b3b4b3d540ad35c5c3cee795a3d6d597f2088b86
Lucky Nurrahmat <lucky.nurrahmat@gmail.com>
2017-01-11 17:27:35 -0500

    add functions to calculator

---
+++ calculator.js
@@ -0,0 +1,23 @@
+function multiply (a,b){
+	return (a * b)
+}
+
+function add (a,b){
+	return (a + b)
+}
+
+function subtract (a,b){
+	return (a - b)
+}
+
+function divide (a,b){
+	return (a / b)
+}
+
+function square(a){
+	return math.pow (a,2)
+}
+
+function cube(a){
+	return math.pow(a,3)
+}
