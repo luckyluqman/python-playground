459d7597941700d837c7ead6235faea8eb8f5e17
Lucky Nurrahmat <lucky.nurrahmat@gmail.com>
2017-01-11 17:31:28 -0500

    add code to call function

--- calculator.js
+++ calculator.js
@@ -21,3 +21,15 @@ function square(a){
 function cube(a){
 	return math.pow(a,3)
 }
+
+console.log("I'm going to use calculator to square operation")
+var x = multiply(5,6)
+console.log(x)
+
+console.log ("I'm going to use calculator to square operation")
+var y = square(5)
+console.log(y)
+
+console.log("I'm going to use calculator to square operation")
+var z = cube(5)
+console.log(z)
